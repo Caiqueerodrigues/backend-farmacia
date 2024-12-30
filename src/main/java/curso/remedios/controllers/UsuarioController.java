d7057ebf6c8145b3a4a31b7dabacf33cb89ad82a
Caíque Rodrigues <caiquerodriguesalmeida@gmail.com>
2024-12-30 01:40:24 -0300

    POST usuario funcionando

--- src/main/java/curso/remedios/controllers/UsuarioController.java
+++ src/main/java/curso/remedios/controllers/UsuarioController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -12,7 +13,6 @@ import curso.remedios.DTO.ResponseDto;
 import curso.remedios.usuario.Usuario;
 import curso.remedios.usuario.UsuarioRepository;
 import curso.remedios.usuario.DTO.DadosUser;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 
@@ -26,11 +26,10 @@ public class UsuarioController {
     @PostMapping
     @Transactional // faz o rollback caso não de certo
     public ResponseEntity<ResponseDto> cadastrar(@RequestBody @Valid DadosUser dados, UriComponentsBuilder uriBuilder) {
-        System.out.println(dados);
         // Buscar o usuário pelo login
         Usuario usuario = (Usuario) repository.findByLogin(dados.login());
-
-        if (usuario == null) {
+        
+        if (usuario != null) {
             var response = new ResponseDto("", "Usuário já cadastrado", "", "");
             return ResponseEntity.status(400).body(response);
         }
@@ -38,7 +37,7 @@ public class UsuarioController {
         var user = new Usuario(dados); //para poder pegar o id usuario cadastrado
         repository.save(user);
         
-        var resposta = new ResponseDto(user, "", "Usuário cadastrado com sucesso!", "");
+        var resposta = new ResponseDto("", "", "Usuário cadastrado com sucesso!", "");
         var uri =  uriBuilder.path("/usuarios/{id}").buildAndExpand(user.getId()).toUri();
         //pegar a url para enviar na resposta, PADRÃO DESING REST
         return ResponseEntity.created(uri).body(resposta);
