cb7db5bf4653cb9d3f399a0e14dfa4ae833aa48d
Caíque Rodrigues <caiquerodriguesalmeida@gmail.com>
2024-12-29 23:20:24 -0300

    recuperando o subject do token

--- src/main/java/curso/remedios/infra/SecurityFilter.java
+++ src/main/java/curso/remedios/infra/SecurityFilter.java
@@ -2,9 +2,11 @@ package curso.remedios.infra;
 
 import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import curso.remedios.DTO.ResponseDto;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -13,21 +15,27 @@ import jakarta.servlet.http.HttpServletResponse;
 //criado o filter, similar ao middleware do express
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private TokenService tokenService;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
         var tokenJWT = recuperarToken(request);
+
+        var subject = tokenService.getSubject(tokenJWT); //pegar o subject do token
         
         filterChain.doFilter(request, response); //chamar o proximo filtro, similar ao next() do express
     }
         
-        private String recuperarToken(HttpServletRequest request) {
-            var authorization = request.getHeader("Authorization"); //pegar o authorization do header
-            
-            if(authorization == null || authorization.isEmpty() || !authorization.startsWith("Bearer ")) {
-                throw new RuntimeException("Token inválido!");
-            }
-            return authorization;
+    private String recuperarToken(HttpServletRequest request) {
+        var authorization = request.getHeader("Authorization"); //pegar o authorization do header
+        
+        if(authorization == null || authorization.isEmpty() || !authorization.startsWith("Bearer ")) {
+            var response = new ResponseDto("", "Token inválido!", "", "");
+            throw new RuntimeException("Token inválido!");
         }
-
+        return authorization;
+    }
 }
--- src/main/java/curso/remedios/infra/TokenService.java
+++ src/main/java/curso/remedios/infra/TokenService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 
@@ -38,5 +39,18 @@ public class TokenService {
     private Instant dataExpiracao() {
         return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
         //pega hora atual do BR, soma duas horas e converte para instant
+    }
+
+    public String getSubject(String token) {
+        try {
+            var algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                .withIssuer("remedios")
+                .build()
+                .verify(token)
+                .getSubject();
+        } catch (Exception e) {
+            throw new RuntimeException("Token inválido ou expirado!");
+        }
     }
 }
